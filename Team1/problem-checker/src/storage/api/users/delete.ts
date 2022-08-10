ec21aec0afb8722c2ee63831160ea186661b5fb6
Edgar-Tumasyan <tumasyan.edgar@mail.ru>
2022-08-10 13:20:33 +0400

    changed users delete function, added check if user exist. If user does not exist, throw error

--- Team1/problem-checker/src/storage/api/users/delete.ts
+++ Team1/problem-checker/src/storage/api/users/delete.ts
@@ -1,9 +1,21 @@
-import { IUser_delete_ReqParam_DTO, IUser_delete_ResBody_DTO } from '../../../common/types/user.types';
+import {
+  IUser_delete_ReqParam_DTO,
+  IUser_delete_ResBody_DTO,
+} from '../../../common/types/user.types';
 import { UserEntity } from '../../db/entities/User.entity';
 import { generateDateInsertPSQLCommand } from '../../../utils/helpers/dateUtils';
 
+export async function deleteById(
+  args: IUser_delete_ReqParam_DTO,
+): Promise<IUser_delete_ResBody_DTO> {
+  const user = await UserEntity.Repository.query(`
+    SELECT * from users
+    WHERE deleted_at is null AND id = '${args.id}'`);
 
-export async function deleteById(args: IUser_delete_ReqParam_DTO): Promise<IUser_delete_ResBody_DTO> {
+  if (!user.length) {
+    throw new Error(`User with id ${args.id} is not found`);
+  }
+
   await UserEntity.Repository.query(`
     UPDATE users
     SET deleted_at = ${generateDateInsertPSQLCommand(new Date())}
