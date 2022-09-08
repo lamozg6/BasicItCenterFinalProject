4ef50db96c4b9752e20b7041e40604eb5cea2231
Narek-Noreyan <noreyan041294@gmail.com>
2022-09-08 18:10:28 +0400

    Complete Tests module

--- Team1/problem-checker/src/modules/tests/dto/Test.delete.ResBody.dto.ts
+++ Team1/problem-checker/src/modules/tests/dto/Test.delete.ResBody.dto.ts
@@ -1,5 +1,5 @@
-import { IDelete_ResBody_DTO } from '../../../common/types/Delete.ResBody.dto';
+import { IDelete_ResBody} from '../../../common/types/Delete.ResBody.dto';
 
 
-export class Test_delete_ResBody_DTO implements IDelete_ResBody_DTO {
+export class Test_delete_ResBody_DTO implements IDelete_ResBody {
 }
